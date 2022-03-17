71e0de3319e4a4d79890653bd036f8dd7b37e2fe
Mark <mark.n.bacon.jr@gmail.com>
2022-03-16 21:05:13 -0400

    commit tree 35b929ef80d361e24841ef4f511e75b4cbb16658

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function callback(accumulator, el){
+        return el += accumulator 
+    };
+
+
+const totalBatteries = batteryBatches.reduce(callback,0)
+
